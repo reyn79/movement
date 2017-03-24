e91b681b428cd14c2154c34f850e3505daf40581
Reynold Ismail <reynold.ismail@team.telstra.com>
2017-03-24 15:55:23 +1100

    Wow thats a lot of comments

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,16 +10,28 @@ var movement = (function() {
   var CONST;
   var CLASSES;
   var ID;
+  /**
+   * [CONST holds misc constants, arrays, api keys etc]
+   * @type {Object}
+   */
   CONST = {
     gak: 'AIzaSyAerv0HM1ZRtnN1hqRHb545CVYhGRGNd_w',
-    bgArray: [ 'bg-home.jpg', 'kickbox-girl.jpg' ],
+    bgArray: ['bg-home.jpg', 'kickbox-girl.jpg'],
     imgPath: 'img/'
   };
+  /**
+   * [CLASSES holds classe reference for DOM elements]
+   * @type {Object}
+   */
   CLASSES = {
     NAVITEM: 'js-nav-item',
     SMOOTHLINK: 'js-smooth-link',
     CAROUSEL: 'carousel-inner'
   };
+  /**
+   * [ID holds ID references for DOM elements]
+   * @type {Object}
+   */
   ID = {
     INFO: 'intro-text',
     CARPAR: 'testimonials'
@@ -29,21 +41,33 @@ var movement = (function() {
    * @type {Object}
    */
   var _private = {
+    /**
+     * [activeNav sets the active page on the main nav]
+     * @return {[void]} [manipulates DOM]
+     */
     activeNav: function() {
       var body = document.querySelector('body');
       var bodyClass = body.getAttribute('class');
       var nav = document.getElementsByClassName(CLASSES.NAVITEM);
       for (var i = nav.length - 1; i >= 0; i--) {
-        if(_helpers.hasClass(nav[i],bodyClass)) {
-          _helpers.addClass(nav[i],'active');
+        if (_helpers.hasClass(nav[i], bodyClass)) {
+          _helpers.addClass(nav[i], 'active');
         }
       }
     },
+    /**
+     * [loadGoogle create a script object to access google api]
+     * @return {[void]} [manipulates DOM]
+     */
     loadGoogle: function() {
       var scr = document.createElement('script');
       scr.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=' + CONST.gak + '&libraries=places&callback=movement.getGoogleReviews');
       document.body.appendChild(scr);
     },
+    /**
+     * [getGoogleReviews description]
+     * @return {[type]} [description]
+     */
     getGoogleReviews: function() {
       // create map element
       var movementhq = { lat: -33.783971, lng: 151.128471 };
@@ -53,14 +77,15 @@ var movement = (function() {
         center: movementhq,
         zoom: 15
       });
+      // create custom marker
       var marker = new google.maps.Marker({
         position: movementhq,
         map: map,
         icon: 'img/logo-pin.png'
       });
-      // Only if testimonial carousel exists
+      // Fire only if testimonial carousel exists
       var testimonial = document.getElementById('testimonials');
-      if(testimonial) {
+      if (testimonial) {
         // request with place ID
         // https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
         var request = {
@@ -73,21 +98,40 @@ var movement = (function() {
         // place object has all data
         // TO-DO: store somewhere? whats the logic?
       }
+      /**
+       * [callback description]
+       * @param  {[type]}   place  [description]
+       * @param  {[type]}   status [description]
+       * @return {Function}        [description]
+       */
       function callback(place, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            _private.fillCarousel(place.reviews);
-          }
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          _private.fillCarousel(place.reviews);
         }
+      }
     },
+    /**
+     * [fillCarousel construct the carousel]
+     * @param  {[array]} arr [array of objects returned from whatever call]
+     * @return {[void]}     [initialises carousel on success]
+     */
     fillCarousel: function(arr) {
+      // carousel parent
       var carPar = document.getElementById(ID.CARPAR);
+      // carousel
       var cars = document.getElementsByClassName(CLASSES.CAROUSEL);
+      // pre compiled handlebars template reference
       var postTemplate = movement.templates['src/html/inc/car-item.hbs'];
       var html = postTemplate(arr);
       cars[0].innerHTML = html;
       carPar.style.display = 'block';
+      // initialise carousel once the html has been set up
       _private.initCarousel();
     },
+    /**
+     * [initCarousel initialises the carousel]
+     * @return {[void]} [manipulates DOM]
+     */
     initCarousel: function() {
       // grab the carousel element
       var myCarousel = document.getElementById('test-car');
@@ -101,7 +145,7 @@ var movement = (function() {
     },
     /**
      * Randomly changes the background image of the body on load
-     * @return {void}
+     * @return {[void]}
      */
     changeBg: function() {
       var bg = CONST.bgArray[Math.floor(Math.random() * CONST.bgArray.length)];
@@ -109,6 +153,10 @@ var movement = (function() {
       var body = document.querySelector('body');
       _helpers.css(body, { 'background-image': imageUrl });
     },
+    /**
+     * [smoothLink finds all elements with smoothlink class and attaches an event]
+     * @return {[void]} [attaches event]
+     */
     smoothLink: function() {
       //Get smooth links
       var smoothLinks = document.getElementsByClassName(CLASSES.SMOOTHLINK);
@@ -118,19 +166,32 @@ var movement = (function() {
         _helpers.addEvent(smoothLinks[i], 'click', _private.smoothScroll);
       }
     },
+    /**
+     * [smoothScroll creates a smooth scroll to element when an anchor tag is clicked]
+     * @return {[void]} [scrolls the page]
+     * TO-DO this is currently not smooth HAHA
+     */
     smoothScroll: function() {
       // scroll into view
       event.preventDefault();
       // get header height
       var headerEl = document.querySelectorAll('header');
       var scrollOffset = headerEl[0].offsetHeight;
+      // where does the link want to go
       var el = document.querySelector(this.getAttribute('href'));
+      // scroll to the element minus the height of the header
       document.documentElement.scrollTop = document.body.scrollTop = el.offsetTop - scrollOffset;
     },
+    /**
+     * [scrollCta fires a CTA modal based on user funnel logic]
+     * @return {[void]} [adds event to element]
+     */
     scrollCta: function() {
       var el = window;
+      // where do you want to scroll to?
       var scrollEl = document.getElementById(ID.INFO);
       var scrollElTop = scrollEl.offsetTop - 55;
+      // add callback to scroll event on window
       _helpers.addEvent(el, 'scroll', callback);
 
       function callback() {
@@ -138,11 +199,12 @@ var movement = (function() {
           // console.log('scrolling baby');
           // get the modal by ID
           var trialModal = document.getElementById('freetrial');
-          // OR initialize and show the modal right away
+          // initialize and show the modal right away
           var myModalInstance = new Modal(trialModal);
           myModalInstance.show();
-          // When shown record this
+          // When shown record this in localstorage
           localStorage.setItem('freetrialshown', true);
+          // remove the event from window
           _helpers.removeEvent(el, 'scroll', callback);
         }
       }
@@ -151,13 +213,19 @@ var movement = (function() {
   var _helpers = {
     /**
      * Takes an element and applies style rules to it
-     * @param  {HTMLElement} el - the element you want to style
+     * @param  {[HTMLElement]} el - the element you want to style
      * @param  {[object]} styles - contains the css rules to apply
-     * @return {void}
+     * @return {[void]}
      */
     css: function(el, styles) {
       for (var property in styles) el.style[property] = styles[property];
     },
+    /**
+     * [addEvent adds events to elements]
+     * @param {[HTML]} el      [element you want to attach event to]
+     * @param {[string]} type    [the event type, click, scroll etc]
+     * @param {[function]} handler [reference to the function you want to fire, can't contain params]
+     */
     addEvent: function(el, type, handler) {
       if (el.attachEvent) {
         // IE9 fix
@@ -167,17 +235,41 @@ var movement = (function() {
         el.addEventListener(type, handler);
       }
     },
+    /**
+     * [removeEvent removes events from elements]
+     * @param {[HTML]} el      [element you want to attach event to]
+     * @param {[string]} type    [the event type, click, scroll etc]
+     * @param {[function]} handler [reference to the function you want to fire, can't contain params]
+     * @return {[void]}         [removes events]
+     */
     removeEvent: function(el, type, handler) {
       if (el.detachEvent) el.detachEvent('on' + type, handler);
       else el.removeEventListener(type, handler);
     },
+    /**
+     * [hasClass checks if an element has a class]
+     * @param {[HTML]} el      [element you want to check for class]
+     * @param  {[string]}  className [name of class]
+     * @return {Boolean}           [return true or false]
+     */
     hasClass: function(el, className) {
       return el.classList ? el.classList.contains(className) : new RegExp('\\b' + className + '\\b').test(el.className);
     },
+    /**
+     * [addClass adds a class to an element]
+     * @param {[HTML]} el        [element you want to add a class to]
+     * @param {[string]} className [name of class]
+     */
     addClass: function(el, className) {
       if (el.classList) el.classList.add(className);
       else if (!hasClass(el, className)) el.className += ' ' + className;
     },
+    /**
+     * [removeClass removes a class from an element]
+     * @param  {[HTML]} el        [element you want to remove class from]
+     * @param  {[string]} className [class you want to remove]
+     * @return {[void]}           [manipulates DOM]
+     */
     removeClass: function(el, className) {
       if (el.classList) el.classList.remove(className);
       else el.className = el.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
@@ -197,7 +289,11 @@ var movement = (function() {
   };
   return _public;
 })();
-
+/**
+ * [init runs all the functionailty we want at start up]
+ * @return {[void]} [runs functions]
+ * TO-DO can i make this more smarter?
+ */
 function init() {
   var ctaShown = localStorage.getItem('freetrialshown');
   movement.activeNav();
